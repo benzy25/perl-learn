df8ba77f624899fda591358f039f6b66a581afd8
Benzy25 <benz_rob@yahoo.com>
2015-05-08 21:26:48 -0400

    find and print line in file with given text

--- newscript.pl
+++ newscript.pl
@@ -7,22 +7,15 @@ sub main {
     my $file = '/Users/Benz/Desktop/beowulf.html';
     open (INPUT, $file) or die "Ha Ha can't fint $file\n";
     
-    my $output = '>output.txt';
-    open (OUTPUT, $output) or die "just can\'t find $output\n";
-    
     while(my $line =<INPUT>) {
-
 
             if ($line =~ /Spear-Danes/) {
 
-            $line =~ s/Days/DAYS/ig;
-                print OUTPUT $line;
+                print $line;
                 
             }
         }
         close (INPUT);
-    close (OUTPUT);
-    close (OUTPUT);
 }
 
 main();
