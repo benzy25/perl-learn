3a28bbb6d9666326927309b4b2a8a5ed108d441f
Rob Benz <benz_rob@yahoo.com>
2015-09-15 11:24:36 -0400

    syntax error? not sure here

--- newscript.pl
+++ newscript.pl
@@ -4,15 +4,19 @@ $|=1;
 
 sub main {
 
-    my $file = '/Users/Benz/Desktop/beowulf.html';
-    open (INPUT, $file) or die "Ha Ha can't fint $file\n";
-    
-    while(my $line = <INPUT>) {
-            if ($line =~ /(Da.*?s)/) {
-                print "$1\n";
-            }
-        }
-        close (INPUT);
+    # * = zero or more of the preceding character ex d* as many as possible
+    # + one or more of the preceding characters as many as possible
+    # *? zero of more of the preceding character, as few as possible
+    # +? one or more of the preceding as few as pssible
+
+   my $text = 'DE19742';
+
+if($text =~ (DE/d{5})/) {
+
+print "Found: '$1'/n";
+
+}
+
 }
 
 main();
