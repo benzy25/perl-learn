d68ba09cea8f18f85fc4c9a2d23a6a96dfebad7f
Rob Benz <benz_rob@yahoo.com>
2015-11-11 16:03:33 -0500

    added print one from file with given string script called print.pl

---
+++ print.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+$|=1;
+
+sub main {
+
+    my $file = '/Users/Benz/Desktop/beowulf.html';
+    open (INPUT, $file) or die "Ha Ha can't fint $file\n";
+    
+    while(my $line =<INPUT>) {
+
+            if ($line =~ /Spear-Danes/) {
+
+                print $line;
+                
+            }
+        }
+        close (INPUT);
+}
+
+main();
+
