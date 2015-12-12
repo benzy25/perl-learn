c12d86e3444d7a0cdd78f82f2440c4a0e80bb50d
Benzy25 <benz_rob@yahoo.com>
2015-12-12 15:25:44 -0500

    autocommit

---
+++ bash/gitme.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+
+REPOS=( 
+~/files/sql 
+~/files/plugs 
+~/files/robbenz 
+~/files/scripts
+~/files/bash_profile
+~/files/diamedical/diamedical
+~/files/diamedical/medmattress
+)
+
+echo What ya wanna do?
+
+read input
+
+if [ $input =  "commit" ]
+then
+    for i in “${REPOS[@]}”
+    do
+        cd $i
+        git add . -A
+        read -p "Commit description: " desc
+        git commit -m "$desc"
+        echo  "Moving to Next REPO...
+         "
+        sleep 1
+    done 
+
+else 
+for i in “${REPOS[@]}”
+do
+    cd $i
+    git $input 
+    echo "Moving to Next REPO...
+     "
+    sleep 1
+    done 
+fi
