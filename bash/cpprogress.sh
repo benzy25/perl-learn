7c0357b66d8012e5e4cfd74d9364120acd81293a
Benzy25 <benz_rob@yahoo.com>
2015-12-13 21:54:06 -0500

    autocommit backup point

---
+++ bash/cpprogress.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+cp -r /Users/Benz/files/scripts /Volumes/Benz_Drive/files
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
