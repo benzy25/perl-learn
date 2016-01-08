8eab40ccc901c8a5b413db18a65fcf9cbd86570f
Rob Benz <benz_rob@yahoo.com>
2016-01-08 16:33:27 -0500

    autocommit backup point

---
+++ data/working.php
@@ -0,0 +1,66 @@
+
+<div class="header-wrap-text-medical-equipment">
+<h2 class="header-wrap-text-medical-equipment-header">Surgical</h2>
+</div>
+
+<h2>Features</h2>
+<h2>Specifications</h2>
+
+<?php
+
+$custom_stuff = array( '_predictive_search_focuskw',
+                       '_yoast_wpseo_focuskw',
+                       '_yoast_wpseo_title'
+                     )
+
+?>
+
+<h2>Specifications</h2>
+<div class="ems-approved-img">
+<img class="ems-img-size" src="/wp-content/themes/x-child-integrity-light/imgs/ems-approved.png" alt="" /></div>
+
+<span class="spotlight-price">$19.50</span>
+
+<a href="/wp-content/pdf/DiaMedical-USA-Rebate-Flyer-Q4-2015.pdf" target="_blank">Click Here For Special Offer</a>
+
+<img class="pdf-img" src="/wp-content/themes/x-child-integrity-light/imgs/pdf-icon.gif"/>
+
+<a href="/product_pdfs/Amico Diagnostic Station Brochure.pdf">
+Click Here for Diagnostic  Brochure</a>
+<img style="padding-top:5px;padding-left:2px;" src="/images/pdf-icon.gif" width="16" height="16" alt="Downloadable Product PDF" />   
+
+<div class="header-wrap-text-mattress"><h2 class="header-wrap-text-mattress-header">Ferno Stretcher Pads</h2></div>
+
+<a href="http://partsonline.diamedicalusa.com/product-category/simulated-iv-bags/"></a>
+
+<a href="#" style="" class="eModal-3">Click Here To View A Full Hill-Rom 835 Hospital Bed Parts List</a>
+
+
+<?php
+//Bash Stuff
+// mogrify -format png *.jpg	400x400
+
+for f in *\ *
+do
+mv "$f" "${f// /-}"
+done
+
+while : 
+do
+clear
+tree ~/whatev
+sleep 1
+done
+
+/*
+[accordion id="my-accordion"] 
+[accordion_item title="Accordion Title" parent_id="my-accordion"]
+Your text here[/accordion_item] 
+[accordion_item title="Accordion Title" parent_id="my-accordion" open="true"]
+Your second accordion text[/accordion_item] 
+[accordion_item title="Accordion Title" parent_id="my-accordion"]
+Your third accordion text[/accordion_item]
+[/accordion]
+*/
+
+?>
