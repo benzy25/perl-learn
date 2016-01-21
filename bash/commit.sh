1700086cb893de98342a76cba3bfc17932c38013
Rob Benz <benz_rob@yahoo.com>
2016-01-21 16:46:59 -0500

    shellcheck edits to commit.sh

--- bash/commit.sh
+++ bash/commit.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 die() { echo "$@" 1>&2 ; exit 1; }
 git add . -A
-read -p "Commit description: " desc  
+read -r -p "Commit description: " desc  
 git commit -m "$desc"
 
 tput setaf 2;echo  Wanna Push?;tput sgr0 
-read ans
-if [ $ans = "yes" ]
+read -r ans
+if [ "$ans" = "yes" ]
 then
     git push
-elif [ $ans = "no" ]
+elif [ "$ans" = "no" ]
 then
    echo "Okay" 
 else die "Fuck You"
 fi
 tput setaf 3;echo Wanna ftp?;tput sgr0
-read ftp 
-if [ $ftp = "yes" ]
+read -r ftp 
+if [ "$ftp" = "yes" ]
 then
     git ftp push
-elif [ $ans = "no" ]
+elif [ "$ans" = "no" ]
 then
    echo "Okay" 
 else die "Fuck You"
