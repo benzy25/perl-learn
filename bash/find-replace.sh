2ab90027658a1bee89d2e00f8eea360443d31b50
Rob Benz <benz_rob@yahoo.com>
2016-02-01 16:33:06 -0500

    got the script working, need to learn how to exacpe characters

--- bash/find-replace.sh
+++ bash/find-replace.sh
@@ -4,4 +4,4 @@ read -r -p "Search For: " FIND
 read -r -p "Replace With: " REPLACE 
 read -r -p "Full Path To File: " FILE 
 
-sed -i -e 's/"${FIND}"/"${REPLACE}"/g' "$FILE" 
+sed -i -e "s/$FIND/$REPLACE/g" "$FILE" 
