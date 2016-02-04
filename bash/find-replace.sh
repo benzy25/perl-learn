b66caa50d5cfa098a1eeffa939c7d8be8a852792
Rob Benz <benz_rob@yahoo.com>
2016-02-04 07:48:22 -0500

    added pushing options to wpcommit.sh

--- bash/find-replace.sh
+++ bash/find-replace.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # this only works with WORDS ONLY
 
--- bash/wpcommit.sh
+++ bash/wpcommit.sh
@@ -1,36 +1,58 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 die() { echo "$@" 1>&2 ; exit 1; }
+
+gr="$(git root)"
+cd "$gr" 
+tput setaf 6;pwd;tput sgr0 
+
 git add . -A
 read -r -p "Commit description: " desc  
 git commit -m "$desc"
 
-tput setaf 2;echo  Wanna Push to BitBucket?;tput sgr0 
-read -r ans
-if [ "$ans" = "yes" ]
+tput setaf 2;echo  Wanna Do Some pushing?;tput sgr0 
+read -r push 
+if [ "$push" = "yes" ]
+then 
+    tput setaf 3;echo  What Branch?;tput sgr0 
+    read -r branch
+    sleep 1
+   
+    tput setaf 2;echo  Wanna Push to BitBucket?;tput sgr0 
+    read -r ans
+    if [ "$ans" = "yes" ]
+    then
+        git push origin "$branch" 
+    elif [ "$ans" = "no" ]
+    then
+        echo "Okay" 
+    else die "Fuck You"
+    fi
+   
+    tput setaf 3;echo Wanna Push to Staging Server?;tput sgr0
+    read -r staging 
+    if [ "$staging" = "yes" ]
+    then
+        git push staging "$branch" 
+    elif [ "$staging" = "no" ]
+    then
+        echo "Okay" 
+    else die "Fuck You"
+    fi
+    
+    tput setaf 3;echo Wanna Push to Production Server?;tput sgr0
+    read -r production 
+    if [ "$production" = "yes" ]
+    then
+        git push production master
+    elif [ "$production" = "no" ]
+    then
+        echo "Okay" 
+    else die "Fuck You"
+    fi
+
+elif [ "$push" = "no" ]
 then
-    git push origin master
-elif [ "$ans" = "no" ]
-then
-   echo "Okay" 
-else die "Fuck You"
-fi
-tput setaf 3;echo Wanna Push to Staging Server?;tput sgr0
-read -r staging 
-if [ "$staging" = "yes" ]
-then
-    git push staging master
-elif [ "$ans" = "no" ]
-then
-   echo "Okay" 
-else die "Fuck You"
-fi
-tput setaf 3;echo Wanna Push to Production Server?;tput sgr0
-read -r production 
-if [ "$production" = "yes" ]
-then
-    git push production master
-elif [ "$ans" = "no" ]
-then
-   echo "Okay" 
+    echo "Okay" 
 else die "Fuck You"
 fi
