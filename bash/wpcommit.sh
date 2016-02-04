d2ca70f26c3f91d89d22acfd4715e1a97f0e91b1
Rob Benz <benz_rob@yahoo.com>
2016-02-04 16:53:54 -0500

    update wpcommit script

--- bash/wpcommit.sh
+++ bash/wpcommit.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 die() { echo "$@" 1>&2 ; exit 1; }
+KILL="Fuck You"
 
 pushing() {
     tput setaf 3;echo  What Branch?;tput sgr0 
@@ -13,25 +14,38 @@ pushing() {
     elif [ "$ans" = "no" ]
     then
         echo "Okay" 
-    else die "Fuck You"
+    else die "$KILL"
+    fi
+}
+
+more() {
+    tput setaf 2;echo More?;tput sgr0 
+    read -r more
+    if [ "$more" = "yes" ]
+    then
+        pushing
+    elif [ "$ans" = "no" ]
+    then
+        echo "Okay" 
+    else die "$KILL"
     fi
 }
 
 gr="$(git root)"
 cd "$gr" 
 tput setaf 6;pwd;tput sgr0 
-
 git add . -A
 read -r -p "Commit description: " desc  
 git commit -m "$desc"
-
 tput setaf 2;echo  Wanna Do Some pushing?;tput sgr0 
 read -r push 
 if [ "$push" = "yes" ]
 then 
     pushing
+    more
+    more
 elif [ "$push" = "no" ]
 then
     echo "Okay" 
-else die "Fuck You"
+else die "$KILL"
 fi
