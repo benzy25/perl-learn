92e2560764ffff7088c6779ce9a79c507fe1dd8e
Rob Benz <benz_rob@yahoo.com>
2016-02-04 16:43:08 -0500

    test

--- bash/wpcommit.sh
+++ bash/wpcommit.sh
@@ -2,6 +2,21 @@
 
 die() { echo "$@" 1>&2 ; exit 1; }
 
+pushing() {
+    tput setaf 3;echo  What Branch?;tput sgr0 
+    read -r branch
+    tput setaf 3;echo  Where to? You can say 'origin', 'staging', or 'production';tput sgr0 
+    read -r ans
+    if [ "$ans" = "origin" ] || [ "$ans" = "staging" ] || [ "$ans" = "production" ]
+    then
+        git push "$ans" "$branch" 
+    elif [ "$ans" = "no" ]
+    then
+        echo "Okay" 
+    else die "Fuck You"
+    fi
+}
+
 gr="$(git root)"
 cd "$gr" 
 tput setaf 6;pwd;tput sgr0 
@@ -14,43 +29,7 @@ tput setaf 2;echo  Wanna Do Some pushing?;tput sgr0
 read -r push 
 if [ "$push" = "yes" ]
 then 
-    tput setaf 3;echo  What Branch?;tput sgr0 
-    read -r branch
-    sleep 1
-   
-    tput setaf 2;echo  Wanna Push to BitBucket?;tput sgr0 
-    read -r ans
-    if [ "$ans" = "yes" ]
-    then
-        git push origin "$branch" 
-    elif [ "$ans" = "no" ]
-    then
-        echo "Okay" 
-    else die "Fuck You"
-    fi
-   
-    tput setaf 3;echo Wanna Push to Staging Server?;tput sgr0
-    read -r staging 
-    if [ "$staging" = "yes" ]
-    then
-        git push staging "$branch" 
-    elif [ "$staging" = "no" ]
-    then
-        echo "Okay" 
-    else die "Fuck You"
-    fi
-    
-    tput setaf 3;echo Wanna Push to Production Server?;tput sgr0
-    read -r production 
-    if [ "$production" = "yes" ]
-    then
-        git push production master
-    elif [ "$production" = "no" ]
-    then
-        echo "Okay" 
-    else die "Fuck You"
-    fi
-
+    pushing
 elif [ "$push" = "no" ]
 then
     echo "Okay" 
