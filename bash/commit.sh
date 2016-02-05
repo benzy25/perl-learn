4a645efa95deae0021b23d1980f1141d6e390cec
Benzy25 <benz_rob@yahoo.com>
2016-02-04 19:48:43 -0500

    blah

--- bash/commit.sh
+++ bash/commit.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 die() { echo "$@" 1>&2 ; exit 1; }
+
 KILL="Fuck You"
 
 pushing() {
@@ -32,33 +33,41 @@ more() {
 }
 
 gitftp() {
-tput setaf 3;echo Wanna ftp?;tput sgr0
-read -r ftp 
-if [ "$ftp" = "yes" ]
-then
-    git ftp push
-elif [ "$ftp" = "no" ]
-then
-   echo "Okay" 
-else die "Fuck You"
-fi
+    tput setaf 3;echo Wanna ftp?;tput sgr0
+    read -r ftp 
+    if [ "$ftp" = "yes" ]
+    then
+        git ftp push
+    elif [ "$ftp" = "no" ]
+    then
+        echo "Okay" 
+    else die "$KILL"
+    fi
 }
 
 gr="$(git root)"
+
 cd "$gr" 
-tput setaf 6;pwd;tput sgr0 
+
+tput setaf 5;pwd;tput sgr0 
+
 git add . -A
 read -r -p "Commit description: " desc  
 git commit -m "$desc"
+
 tput setaf 2;echo  Wanna Do Some pushing?;tput sgr0 
 read -r push 
 if [ "$push" = "yes" ]
 then 
     pushing
-    more
-    more
+    until [ "$more" = "no" ]
+    do
+        more
+    done
 elif [ "$push" = "no" ]
 then
     echo "Okay" 
 else die "$KILL"
 fi
+
+
--- bash/flow_pull.sh
+++ bash/flow_pull.sh
@@ -5,30 +5,14 @@ REPOS=(
 ~/parts_online
 )
 
-
 tput setaf 2;echo "Push or Pull?"; tput sgr0
 
 read input
 
-if [ $input =  "push" ]
-then
-
 for i in "${REPOS[@]}"
 do
     cd "$i"
     tput setaf 6;pwd;tput sgr0 
-    git push origin master 
+    git "$input" origin master 
     sleep 1
-    done 
-elseif [ "$input" = "pull" ]
-then
-    
-for i in "${REPOS[@]}"
-do
-    cd "$i"
-    tput setaf 6;pwd;tput sgr0 
-    git pull origin master 
-    sleep 1
-    done 
-fi
-
+done 
