d639da169c702e043fd2a3f61ab24aa13e25c70c
Rob Benz <benz_rob@yahoo.com>
2016-06-15 12:20:17 -0400

    autocommit backup point

---
+++ bash/wp_users.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+users() {
+
+wp user list --fields=user_email | grep 'hospital' >> /Users/jeffreyambrose/Desktop/email-tmp/hospital.csv
+wp user list --fields=user_email | grep -v 'hospital' >> /Users/jeffreyambrose/Desktop/email-tmp/nursing.csv
+}
+
+mkdir ~/Desktop/email-tmp/
+cd /Applications/MAMP/htdocs/parts_online
+users
+sleep 1
+cd /Applications/MAMP/htdocs/medmattress
+users
+
+
+cd ~/Desktop/email-tmp/
+for s in ./*.csv
+do
+    grep -E -o "\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b" "$s" | sort -u > "${s}.csv"
+sleep 1
+done
+
+for t in ./*.csv
+do
+    mv "$t" "${t//.csv.csv/.csv}"
+done
+
+tput setaf 2; echo "type done when you are finished importing";tput sgr0
+read input
+if [ "$input" = "done" ]
+then 
+    rm -rf ~/Desktop/email-tmp/
+fi
+
