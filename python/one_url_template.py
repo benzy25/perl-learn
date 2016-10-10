0a11aeef3b34397ddaa365f997337c190b87c578
Rob Benz <benz_rob@yahoo.com>
2016-10-10 07:57:06 -0400

    some updates to email script and python web scraping

--- python/one_url_template.py
+++ python/one_url_template.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://www.traillink.com/traillist.aspx?zipcode=48009&distance=50&length=0|99999"
+url = "https://www.inprocorp.com/colorsandfabrics/clickeze-privacy-systems/Bronze"
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, "lxml")
 
-g_data = soup.find_all("div", {"class": "item"})
+g_data = soup.find_all("li", {"class": "colorItem"})
 
 for item in g_data:
-	print item.contents[0].find_all("p", {"class": "name"})[0].text
-	print item.contents[0].find_all("img", {"class": "wp-post-image"})[0].get("src")
-	print item.contents[0].find_all("td", {"class": "col2"})[0].text
+        print item("img", {"class": "colorImage"})[0].get("src") +' |||'
+        print item("h3")[0].text.replace(' ', '' , 1 ) +' ||'
+        print item("p")[0].text.replace(' ', '' , 1 ) +' |'
+        print '\n'
