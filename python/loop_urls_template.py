1cef57c2a83718e97ca56e53c85d416eb76956bc
Benzy25 <benz_rob@yahoo.com>
2016-10-25 20:30:46 -0400

    python scripts update

--- python/loop_urls_template.py
+++ python/loop_urls_template.py
@@ -6,17 +6,41 @@ with open('urls.txt') as inf:
 	for url in urls:
 		r = requests.get(url)
 		soup = BeautifulSoup(r.content, "lxml")
-		print soup.find_all("h1", {"class": "trailName"})[0].text
+                print soup.find_all("h1", {"class": "trailName"})[0].text.replace("\n", "") +"|"
                 try:
-                    print soup.find_all("table", {"class": "details"}).str.replace('\n', ' ')
+                    print soup.find_all("table", {"class": "details"}).str.replace('\n', ' ') +"|"
                 except:
                     pass
                 try:
-                    print soup.find_all("h3")[0].text
+                    print soup.find_all("div", {"id": "divDesc"})[0].text +"|"
                 except:
                     pass
                 try:
-                    print soup.find_all("p")[0].text
+                    print soup.find_all("div", {"id": "divPark"})[0].text +"|"
+                except:
+                    pass
+                try:
+                    print soup.find_all("a", {"class": "mainTrailImage"})[0].get('href') +"|"
+                except:
+                    pass
+                try:
+                    print soup.find_all("a", {"class": "btnviewmap"})[0].get('href') +"|"
+                except:
+                    pass
+                try:
+                    print soup.find_all("a", {"id": "ContentPlaceHolderDefault_rootMainContent_mainContent_traildetailmain_3_btnViewAllReviews"})[0].get('href') +"|"
+                except:
+                    pass
+                try:
+                    print soup.find_all("div", {"id": "hotelVisible"})[0].text.replace("                        ", "") +"|"
+                except:
+                    pass
+                try:
+                    print soup.find_all("table", {"class": "details"})[0].text +"|"
+                except:
+                    pass
+                try:
+                    print soup.find_all("div", {"id": "trailVisible"})[0].text.replace("                               ", "") +"|"
                 except:
                     pass
                 print "\n"
