c2400c9b772f2efe2c5f0d8dfe6672f421d86cd8
Rob Benz <benz_rob@yahoo.com>
2017-06-30 15:27:34 -0400

    autocommit backup point

--- bash/filezilla.sh
+++ bash/filezilla.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 rm -rf /Applications/FileZilla.app/
-mv ~/Documents/FileZilla.app/ /Applications/
-rm -rf ~/Documents/FileZilla*
+mv ~/Downloads/FileZilla.app/ /Applications/
+rm -rf ~/Downloads/FileZilla*
 open /Applications/FileZilla.app/
