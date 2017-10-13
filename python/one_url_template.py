963bb78e49dbd343bc34ab6887bef1576d49468f
Rob Benz <benz_rob@yahoo.com>
2017-10-13 13:41:36 -0400

    autocommit backup point

--- python/one_url_template.py
+++ python/one_url_template.py
@@ -1,17 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-zpid = "18429834"
-url = "http://www.zillow.com/homes/" + zpid + "_zpid/"
+#zpid = "18429834"
+#url = "http://www.zillow.com/homes/" + zpid + "_zpid/"
+url = "http://www.roscoemedical.com/?itemNumber=MSV503"
 
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, "lxml")
 
-g_data = soup.find_all("div", {"class": "home-summary-row"})
+g_data = soup.find_all("div", {"id": "OverViewTab"})
 
 for item in g_data:
         #print item("img", {"class": "colorImage"})[0].get("src") +' |||'
         print item("span")[0].text
         #print item("p")[0].text.replace(' ', '' , 1 ) +' |'
+        print '\n'
+
+bg_data = soup.find_all("div", {"class": "magnifyarea"})
+
+for items in bg_data:
+        print items("img")[0].src
         print '\n'
