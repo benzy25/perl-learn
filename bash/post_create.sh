8fe26055cede2f7971924f393f3d902369fb9840
Benzy25 <benz_rob@yahoo.com>
2018-08-05 17:53:42 -0400

    tweak to post create

--- bash/post_create.sh
+++ bash/post_create.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e 
+
+echo "listing post types... "
+wp post-type list
+
 tput setaf 2;echo "Post type: "; tput sgr0
 read -r POST 
 
 tput setaf 2;echo "How many posts: "; tput sgr0
 read -r COUNT
-
-tput setaf 2;echo "Unique titles? [y/n]: "; tput sgr0
-read -r UNIQUE 
 
 #wp user list | grep 'insideout'
 wp user list
@@ -14,10 +15,13 @@ wp user list
 tput setaf 2;echo "User ID (int): "; tput sgr0
 read -r ID
 
-#for i in {1..$(echo "$COUNT")}
+tput setaf 2;echo "Unique titles? [y/n]: "; tput sgr0
+read -r UNIQUE 
+
 for ((i = 1; i <= COUNT; i++))
 do
-    if UNIQUE = y
+    if [ "$UNIQUE" =  "y" ] || [ "$UNIQUE" = "yes" ]
+    then
         tput setaf 2;echo "Title $i: "; tput sgr0
         read -r TITLE 
         wp post create  --post_type="$POST" --post_content="hey" --post_author="$ID" --post_title="$TITLE" --post_status="publish" 
